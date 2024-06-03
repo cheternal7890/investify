084bd0c50ba426186ff5836ec91054f414db7c71
cheternal7890 <cesar.jimenez.btw@gmail.com>
2024-06-03 16:12:16 -0400

    Added index.js for app functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+const username = document.querySelector("#username");
+
+const settledCash = document.getElementById("settledCash");
+const buyingPower = document.getElementById("buyingPower");
+const dividends = document.getElementById("dividends");
+
