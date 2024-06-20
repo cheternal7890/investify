2b5f4c1e807c29470b8fdc421805341baf5c004a
cheternal7890 <cesar.jimenez.btw@gmail.com>
2024-06-19 23:09:44 -0400

    Reverted back to working old version

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, "/")));
+app.use(express.static(path.join(__dirname, "/css")));
 
 app.get("/", async (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
