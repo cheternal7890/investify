dd9aaace2265ee2b0806fc08c4c158d3f7bdfc2a
cheternal7890 <cesar.jimenez.btw@gmail.com>
2024-06-19 23:11:05 -0400

    git commit

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
 
-app.use(express.static(path.join(__dirname, "/css")));
+// app.use(express.static(path.join(__dirname, "/css")));
 
-app.get("/", async (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-})
+// app.get("/", async (req, res) => {
+//     res.sendFile(path.join(__dirname, "index.html"));
+// })
 
-app.get("/positions", async (req, res) => {
-    res.sendFile(path.join(__dirname, "/pages/positions.html"));
-})
+// app.get("/positions", async (req, res) => {
+//     res.sendFile(path.join(__dirname, "/pages/positions.html"));
+// })
 
-app.get("/updates", async (req, res) => {
-    res.sendFile(path.join(__dirname, "/pages/updates.html"));
-})
+// app.get("/updates", async (req, res) => {
+//     res.sendFile(path.join(__dirname, "/pages/updates.html"));
+// })
 
-app.listen(3000);
+// app.listen(3000);
 
-module.exports = app;
+// module.exports = app;
