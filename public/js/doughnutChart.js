a2424d98a48286218f3baeef3497c16c2b053e98
cheternal7890 <cesar.jimenez.btw@gmail.com>
2024-07-12 20:21:33 -0400

    Added visual graphs using Chart.js and fixed font inconsistencies

---
+++ public/js/doughnutChart.js
@@ -0,0 +1,16 @@
+var chrt = document.getElementById("doughnutChart").getContext("2d");
+var chartId = new Chart(chrt, {
+   type: 'doughnut',
+   data: {
+      labels: ["Stocks", "Securities", "Holdings", "Exchange Traded Funds"],
+      datasets: [{
+      label: "Percentage of Portfolio",
+      data: [25, 25, 15, 35],
+      backgroundColor: ['#8ecae6', '#023047', '#ffb703', '#fb8500'],
+      hoverOffset: 5
+      }],
+   },
+   options: {
+      responsive: true,
+   },
+});
